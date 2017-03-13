3c63b1398d3aab96389c6d9f6a0eb058c780c74e
Will <Will@Will-Custom>
2017-03-13 12:47:42 +0000

    update

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -32,6 +32,16 @@ public class ScotlandYardModel implements ScotlandYardGame {
 			PlayerConfiguration... restOfTheDetectives) {
             this.rounds = requireNonNull(rounds);
             this.graph = requireNonNull(graph);
+            
+            if (rounds.isEmpty()) {
+            throw new IllegalArgumentException("Empty rounds");
+            }
+            
+            if (graph.isEmpty()) {
+            throw new IllegalArgumentException("Empty graph");
+            }
+            
+            throw new RuntimeException("Implement me");
 		// TODO
 	}
 
