b4f5a92279481e8e66950b049c80271d35e7285e
Louis Heath <lh16421@bristol.ac.uk>
2017-03-16 11:05:51 +0000

    bant

--- cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
+++ cw-model/src/main/java/uk/ac/bris/cs/scotlandyard/model/ScotlandYardModel.java
@@ -1,18 +1,41 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.Objects.requireNonNull;
+import static uk.ac.bris.cs.scotlandyard.model.Colour.Black;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.Double;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.Secret;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+import java.util.function.Consumer;
+import uk.ac.bris.cs.gamekit.graph.Edge;
 import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
 
 // TODO implement all methods and pass all tests
 public class ScotlandYardModel implements ScotlandYardGame {
-
+    
+        List<Boolean> rounds;
+        Graph<Integer, Transport> graph;
+        
 	public ScotlandYardModel(List<Boolean> rounds, Graph<Integer, Transport> graph,
 			PlayerConfiguration mrX, PlayerConfiguration firstDetective,
 			PlayerConfiguration... restOfTheDetectives) {
-		// TODO
+	
+ 
+            
+            
+            this.rounds = requireNonNull(rounds);
+            this.graph = requireNonNull(graph);
 	}
 
 	@Override
