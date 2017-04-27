e102ac2acc166e446d37bcba7931ecca70680f34
Will Hawkins <wh16862@my.bristol.ac.uk>
2017-04-27 13:06:40 +0100

    double Moves fixed

--- cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Heathkins.java
+++ cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/Heathkins.java
@@ -6,6 +6,7 @@ import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
 import uk.ac.bris.cs.scotlandyard.model.Colour;
 import static uk.ac.bris.cs.scotlandyard.model.Colour.Black;
 import uk.ac.bris.cs.scotlandyard.model.Player;
+import uk.ac.bris.cs.scotlandyard.ui.ai.HeathkinsD2.DetectiveV2;
 import uk.ac.bris.cs.scotlandyard.ui.ai.HeathkinsMrX.MrXAI;
 
 @ManagedAI("Heathkins")
@@ -15,6 +16,6 @@ public class Heathkins implements PlayerFactory {
 	@Override
 	public Player createPlayer(Colour colour) {
             if(colour == Black) return new MrXAI();
-            else return new HeathkinsD3.DetectiveV3();
+            else return new DetectiveV2();
 	}
     }        
